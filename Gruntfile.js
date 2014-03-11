219c5070ab63176bf802a5ac1c44759b78e9d6cd
Pascal Precht <pascal.precht@googlemail.com>
2014-03-11 15:41:24 +0100

    chore(*): adds grunt-conventional-changelog plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,6 +135,12 @@ module.exports = function (grunt) {
             },
         },
 
+        changelog: {
+            options: {
+                dest: 'CHANGELOG.md'
+            }
+        },
+
         delta: {
 
             gruntfile: {
